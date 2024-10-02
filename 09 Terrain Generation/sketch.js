3d03e7e07a882a2913f620e27491e394f1a97235
Masterxxmits <idrismitsaal@gmail.com>
2024-10-02 12:16:57 -0600

    TERRAIN .2

--- 09 Terrain Generation/sketch.js
+++ 09 Terrain Generation/sketch.js
@@ -3,7 +3,7 @@
 // Oct. 1, 2024
 //
 
-let rectwidth = 20;
+let rectWidth = 20;
 let terrainInterval = .01;
 let terrainTime = 5;
 
@@ -14,15 +14,48 @@ function setup() {
 
 function draw() {
   background(220);
+  terrainTime = 5+frameCount/50;
   terrain();
-
+  drawFlag();
 }
 
 function terrain(){
-  for(let x = 0; x <= width; x += rectwidth){
+  let highestTerrain = 0
+  let highestTerrainx = 0
+  let averageHeight = 0
+  for(let x = 0; x <= width; x += rectWidth){
     noFill();
-    let rectHeight = 
+    let rectHeight = noise(terrainTime);
+    rectHeight = map(rectHeight,0,1,10,999);
+    if(rectHeight > highestTerrain){
+      highestTerrain = rectHeight
+      highestTerrainx = x
+    }
+    //averageHeight =
+    rect(x,height,rectWidth,-rectHeight);
+    terrainTime += terrainInterval
 
   }
+  drawFlag(highestTerrainx,height-highestTerrain);
+}
+
+function keyPressed(){
+  if(keyCode === RIGHT_ARROW){
+    
+    rectWidth += 1
+    }
+
+  if(keyCode === LEFT_ARROW){
+    if(rectWidth > 1){
+    rectWidth -= 1
+  }
+}
+}
+
+function drawFlag(x,y){
+  stroke(1);
+  fill(255,0,0);
+  line(x,y,x,y-50);
+  rect(x,y-50,20,20);
 
 }
