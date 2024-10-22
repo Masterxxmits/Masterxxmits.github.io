f0e77a451f4f7b975f6a31e9bb46495facc328fb
Masterxxmits <idrismitsaal@gmail.com>
2024-10-22 12:16:48 -0600

    CAR

--- 22 CARS CARS CARS/sketch.js
+++ 22 CARS CARS CARS/sketch.js
@@ -5,6 +5,8 @@
 let lineA = 0;
 let lineB = 20;
 let myCar;
+let eastbound = [];
+let westbound = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,6 +16,12 @@ function setup() {
 function draw() {
   background(220);
   drawRoad();
+  for(let i = 0; i<eastbound.length; i++){
+    eastbound[i].action();
+  }
+  for(let i = 0; i<westbound.length; i++){
+    westbound[i].action();
+  }
 }
 
 function drawRoad(){
@@ -30,17 +38,75 @@ function drawRoad(){
   lineA = 0;
   lineB = 20;
 }
+
+function mouseClicked(){
+  if (keyIsPressed&&keyCode === SHIFT){
+    westbound.push(new Vehicle(0,random(height/2.1,height/2.3),0));
+  }
+  else{
+    eastbound.push(new Vehicle(width,random(height/1.6,height/1.8),1));
+  }
+}
 class Vehicle{
-  constructor(type, x, y, color, direction,){
-    this.type = type;
+  constructor(x, y, direction){
+    this.type = int(random(2));
     this.x = x;
     this.y = y;
     this.color = color(random(255), random(255), random(255));
     this.direction = direction;
-    this.xSpeed = random(1,200);
-    this.cars = [];
+    this.xSpeed = random(1,15);
+   
   }
 
+  action(){
+    this.move();
+    this.display();
+  }
+  display(){
+    if (this.type===0){
+      this.drawCar();
+    }
+    else if (this.type===1){
+      this.drawTruck();
+    }
+  }
+
+  
+  drawCar(){
+    fill(this.color);
+    rect(this.x,this.y,100,40);
+  }
+  drawTruck(){
+    fill(this.color);
+    ellipse(this.x,this.y,100,40);
+  }
+
+  move(){
+    if(this.direction === 0){
+      this.x += this.xSpeed;
+      if(this.x > width){
+        this.x = 0;
+      }
+    }
+    else if(this.direction === 1){
+      this.x -= this.xSpeed;
+      if(this.x < 0){
+        this.x  = width;
+      }
+   }
+  }
+  speedUp(){
+    if(this.direction === 0){
+      
+    }
+    
+  }
+  speedDown(){
+
+  }
+  changeColor(){
+
+  }
 }
 
   
