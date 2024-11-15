4d46268649f9605bf67a7bf279e647f15dcf7d00
Masterxxmits <idrismitsaal@gmail.com>
2024-11-15 12:16:15 -0600

    TREE

--- 30 Tree/sketch.js
+++ 30 Tree/sketch.js
@@ -4,18 +4,22 @@
 
 
 let scale = 30;
+let dep = 5;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
+  //stop seizures
   seed = random(100);
 }
 function draw() {
   background(255);
   randomSeed(seed);
   drawTree(width/2, height*0.9, 90, 6);
-  drawLeaf(width/2, height*0.9, 90, 6);
+  
 }
 function drawLine( x1, y1, x2, y2, depth) {
+//Thick lines
+  strokeWeight(depth);
 //draw a line segment connecting (x1,y1) to (x2,y2)
   line(x1, y1, x2, y2);
 }
@@ -25,25 +29,34 @@ function drawTree(x1, y1, angle, depth) {
     let y2 = y1 - sin(radians(angle))*depth*scale; //using trig ratios. Get shorter based on depth
     let den = map(mouseX, 0, width, 1, 30 );
     drawLine(x1, y1, x2, y2, depth);
+    if(depth <= dep){
+      drawLeaf(x2, y2, depth*15);
+    }
     //for a 2-branch tree:
     drawTree(x2, y2, angle-den, depth-1);
     drawTree(x2, y2, angle+den, depth-1);
     //3 branch:
     drawTree(x2, y2, angle, depth-1);
+  
+  
   }
-  //else if(depth < 5){
-    //drawLeaf();
-  //}
+}
+
+//More leaf or less leaf
+function keyPressed(){
+  if(keyCode === 90){
+    dep -= 1;
+
+  }
+  if(keyCode === 88){
+    dep += 1;
+
+  }
 }
 
 //draw leaves
-function drawLeaf(x1, y1, angle, depth){
-  let x2 = x1 + cos(radians(angle))*depth*scale;
-  let y2 = y1 - sin(radians(angle))*depth*scale;
+function drawLeaf(x1, y1, d){
   fill(random(255), random(255), random(255));
-  let cir = map(mouseX, 0, width, 1, 30);
-  circle(x2, y2, d);
-  drawLeaf(x2, y2, angle-cir, depth-1);
-  drawLeaf(x2, y2, angle+cir, depth-1);
+  circle(x1, y1, d);
 }
 
